1b1174df89f32991738c067b801006548b4e0319
tiasenjaya <tiasenjaya@gmail.com>
2025-03-06 15:46:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ if support_filter != "All":
 col_space, col_main, col_space2 = st.columns([0.2, 1, 0.2])  # Ruang kiri & kanan lebih kecil
 
 with col_main:
-    st.title(f"📊 Dashboard Tiket SUPPORT")  # Nama Sheet Diganti dengan Fixed Title
+    st.title(f"📊 PERFORMANCE DASHBOARD")  # Nama Sheet Diganti dengan Fixed Title
     
     col1, col2, col3, col4 = st.columns(4)
     
